313467faf323f32b047a0881a9b1c4feb26b9670
John Walker Bolding <johnwalkerbolding@John-Walkers-MacBook-Pro-3.local>
2026-02-11 16:01:05 -0500

    build dataframe

--- features/test.py
+++ features/test.py
@@ -1,16 +1,9 @@
+import momentum
+import volatility
+import returns
+import pandas as pd
+import numpy as np
 
-from volatility import calculate_volatility
+dataframe = pd.DataFrame(columns=["date","Ticker","5dayreturn","63daymomentum","20dayolatility","Forward return"])
 
-# numlist = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,45,6,7,89,12,23,34,45,5,6]
-
-
-
-
-# stddev = compute_individual_volatility(numlist)
-
-# print(stddev)
-
-
-vol_frame = calculate_volatility("return1day.parquet")
-
-print(vol_frame.head())
+print(dataframe)
